93dc291eb7719266179c82375791e4cec269db04
Fellipe Andrade <fellipe.andrade@afya.com.br>
2023-02-01 13:22:59 -0300

    feat: search ips by url string

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate returns cli application ready to exec
 func Generate() *cli.App {
@@ -8,5 +14,32 @@ func Generate() *cli.App {
 	app.Name = "Simple CLI App"
 	app.Usage = "Search IPs and Names of Internet Servers."
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search IPs of Internet Servers.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"simple-cli-app/app"
 )
 
 func main() {
-	fmt.Println("Start Point!")
-
 	application := app.Generate()
 	if err := application.Run(os.Args); err != nil {
 		log.Fatal(err)
